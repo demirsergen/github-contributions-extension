5df6f978346d0b2f6de9cf6e7bd9544f90311636
Sergen Demir <dmrsrgn@gmail.com>
2023-11-23 23:47:15 -0600

    implement conditional rendered UI, ability to change username and update UI

--- src/app/page.js
+++ src/app/page.js
@@ -8,40 +8,43 @@ import Box from './components/Box';
 export default function Home() {
   const [totalContributions, setTotalContributions] = useState(0);
   const [dailyContributions, setDailyContributions] = useState([]);
+  const [username, setUsername] = useState('');
 
-  useEffect(() => {
-    const fetchContributions = async () => {
-      try {
-        const response = await retrieveContributionData(
-          'demirsergen'
-        );
-        setTotalContributions(
-          response.data.user.contributionsCollection
-            .contributionCalendar.totalContributions
-        );
-        setDailyContributions(
-          response.data.user.contributionsCollection.contributionCalendar.weeks.slice(
-            -1
-          )
-        );
+  const fetchContributions = async () => {
+    try {
+      const response = await retrieveContributionData(username);
 
-        console.log(
-          response.data.user.contributionsCollection.contributionCalendar.weeks.slice(
-            -1
-          )
-        );
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchContributions();
-  }, []);
+      setTotalContributions(
+        response.data.user.contributionsCollection
+          .contributionCalendar.totalContributions
+      );
+      setDailyContributions(
+        response.data.user.contributionsCollection.contributionCalendar.weeks.slice(
+          -1
+        )
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="App">
         <h1>GitHub Contributions for the Last Week</h1>
+        <input
+          type="text"
+          value={username}
+          placeholder="Enter username..."
+          onChange={(e) => setUsername(e.target.value)}
+          className="text-black px-1"
+        />
+        <button
+          onClick={fetchContributions}
+          className="bg-teal-500 px-1"
+        >
+          Get Contributions
+        </button>
         <div className="flex gap-1">
           {dailyContributions[0]?.contributionDays.map(
             (day, index) => (
@@ -55,20 +58,27 @@ export default function Home() {
           )}
         </div>
         <div className="bg-teal-800 mt-2 p-2 text-white rounded flex flex-col gap-2">
-          <h1 className="font-bold mb-2">
-            Detailed Look to your week:
-          </h1>
-          {dailyContributions[0]?.contributionDays.map(
-            (day, index) => (
-              <div key={index} className="bg-teal-700">
-                <p>{moment(day.date).format('dddd, MMMM Do')}</p>
-                <p>Contributions: {day.contributionCount}</p>
-              </div>
+          {dailyContributions[0]?.contributionDays ? (
+            dailyContributions[0]?.contributionDays.map(
+              (day, index) => (
+                <>
+                  <h1 className="font-bold mb-2">
+                    Detailed Look to your week:
+                  </h1>
+                  <div key={index} className="bg-teal-700">
+                    <p>{moment(day.date).format('dddd, MMMM Do')}</p>
+                    <p>Contributions: {day.contributionCount}</p>
+                  </div>
+                  <h1 className="font-bold mt-2">
+                    Total contributions this year:{' '}
+                    {totalContributions}
+                  </h1>
+                </>
+              )
             )
+          ) : (
+            <h1>Nothing to see...</h1>
           )}
-          <h1 className="font-bold mt-2">
-            Total contributions this year: {totalContributions}
-          </h1>
         </div>
       </div>
     </main>
